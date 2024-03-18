ff8fcb5418c9ca625e9d51734937cbfa8580b776
soheilMansoori <ahadmansoori369@gmail.com>
2024-03-18 21:52:21 +0330

    create preloade"

---
+++ src/components/Preloader/preloader.jsx
@@ -0,0 +1,10 @@
+export default function preloader() {
+    return (
+        <div>
+            <div className="preloader d-block">
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    )
+}
