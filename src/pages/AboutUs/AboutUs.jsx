f699139caf74f3be0fcbd1407c4c2bff582d7f45
soheilMansoori <ahadmansoori369@gmail.com>
2024-03-23 16:46:23 +0330

    create and fix students comments section in about us page

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,7 +1,9 @@
-import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
-import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
+import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
 import BestTeachers from "./components/BestTeachers/BestTeachers";
+import NewsLetter from '../../components/NewsLetter/NewsLetter';
+import Footer from "../../components/Footer/Footer";
+import StudentsComments from "./components/StudentsComments/StudentsComments";
 
 export default function AboutUs() {
     return (
@@ -9,6 +11,8 @@ export default function AboutUs() {
             <Navbar />
             <Breadcrumb title='درباره ما چه میگویند؟' pageName='درباره ما' />
             <BestTeachers />
+            <StudentsComments />
+            <NewsLetter />
             <Footer />
         </>
     )
---
+++ src/pages/AboutUs/components/StudentsComments/StudentsCommentBox/StudentsCommentBox.jsx
@@ -0,0 +1,18 @@
+export default function StudentsCommentBox() {
+    return (
+        <div className="testimonial-detail">
+            <div className="client-detail-box">
+                <div className="pic">
+                    <img src="./images/teachers/teacher-1.jpg" />
+                </div>
+                <div className="client-detail">
+                    <h3 className="testimonial-title">نازنین عبدی</h3>
+                    <span className="post">طراح وب</span>
+                </div>
+            </div>
+            <p className="description">
+                " چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. "
+            </p>
+        </div>
+    )
+}
