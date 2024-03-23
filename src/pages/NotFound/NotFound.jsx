cf350786718685c1371e6c640b40aa5b46e0c5cd
soheilMansoori <ahadmansoori369@gmail.com>
2024-03-23 21:05:27 +0330

    create and fix NotFound page

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,28 @@
+import Navbar from '../../components/Navbar/Navbar';
+import NewsLetter from '../../components/NewsLetter/NewsLetter';
+import Footer from '../../components/Footer/Footer';
+import { Link } from 'react-router-dom';
+import './NotFound.css'
+
+export default function NotFound() {
+    return (
+        <>
+            <Navbar />
+            <section className="error-wrap">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-6 col-md-10">
+                            <div className="text-center">
+                                <img src="/images/NotFound/404.png" className="img-fluid" />
+                                <p>شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد.</p>
+                                <Link to="/" className="btn btn-theme">بازگشت به خانه</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <NewsLetter />
+            <Footer />
+        </>
+    )
+}
--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,13 @@
 import Home from "../pages/Home/Home"
 import AboutUs from "../pages/AboutUs/AboutUs"
 import ContactUs from "../pages/ContactUs/ContactUs"
+import NotFound from "../pages/NotFound/NotFound"
 
 const router = [
     { path: '/', element: <Home /> },
     { path: '/about-us', element: <AboutUs /> },
     { path: '/contact-us', element: <ContactUs /> },
+    { path: '*', element: <NotFound /> },
 ]
 
 export default router
