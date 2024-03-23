547631bd645054610d03c4b9f68bd5e29a73bb6d
soheilMansoori <ahadmansoori369@gmail.com>
2024-03-23 15:38:27 +0330

    just create about us page

---
+++ src/pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,7 @@
+export default function AboutUs() {
+    return (
+        <div>
+            this is about page
+        </div>
+    )
+}
--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,9 @@
+import AboutUs from "../pages/AboutUs/AboutUs"
 import Home from "../pages/Home/Home"
 
 const router = [
-    { path: '/', element: <Home /> }
+    { path: '/', element: <Home /> },
+    { path: '/about-us', element: <AboutUs /> },
 ]
 
 export default router
