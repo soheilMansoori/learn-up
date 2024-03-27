8e58f05cda4a1c46d024a3e41a70a4beb0ec6bba
soheilMansoori <ahadmansoori369@gmail.com>
2024-03-28 00:04:19 +0330

    fix bug when user change the page scroll to top

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import { useRoutes } from 'react-router-dom'
+import { useEffect } from 'react';
+import { useRoutes, useLocation } from 'react-router-dom'
 import router from './router/router'
 
 function App() {
-  const routes = useRoutes(router)
+  const location = useLocation()
+  const routes = useRoutes(router); // page routes
+
+  useEffect(() => { // run when pages change and scroll to top of page
+    window.document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+    })
+  }, [location]);
+
   return routes;
 }
 
