904410807a9add0d279481b9f256db4a30965671
soheilMansoori <ahadmansoori369@gmail.com>
2024-03-28 00:43:58 +0330

    add react query to project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import App from './App';
+
+
+// react query devtools 
+import { ReactQueryDevtools } from 'react-query/devtools'
+
 
 // redux configs 
 import { Provider } from 'react-redux'
 import store from './redux/store'
+
 
 // Custom CSS
 import './css/style.css';
@@ -13,12 +20,18 @@ import './css/style.css';
 // Custom Color Option  
 import './css/colors.css'
 
+// react query configs
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter >
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        {/* react query devtools component */}
+        <ReactQueryDevtools position='top-right' initialIsOpen={false} /> 
+      </QueryClientProvider>
     </Provider>
   </BrowserRouter>
 );
