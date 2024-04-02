19c8120401a79735e4c884d8c0ef4de01796bb59
soheilMansoori <ahadmansoori369@gmail.com>
2024-04-02 20:16:05 +0330

    add new option for useInfiniteScroll hook

--- src/hooks/useInfiniteScroll.js
+++ src/hooks/useInfiniteScroll.js
@@ -2,8 +2,12 @@ import { useState, useEffect } from 'react';
 import { useInfiniteQuery } from 'react-query';
 
 export default function useInfiniteScroll(key, fetcher) {
+    // states
     const [hasNextPage, setHasNextPage] = useState(false);
     const [mainData, setMainData] = useState([]);
+    const [allItemsLength, setAllItemsLength] = useState(null);
+
+    // react query 
     const { data, isFetching, fetchNextPage, ...rest } = useInfiniteQuery(
         key,
         fetcher,
@@ -25,7 +29,9 @@ export default function useInfiniteScroll(key, fetcher) {
         // set next page
         const hasNextPage = data?.pages.every(page => page.next);
         setHasNextPage(hasNextPage);
+        const allItemsLength = data?.pages[0].items
+        setAllItemsLength(allItemsLength)
     }, [data]);
 
-    return { hasNextPage, mainData, isFetching, fetchNextPage , ...rest};
+    return { hasNextPage, mainData, isFetching, fetchNextPage, allItemsLength, ...rest };
 }
