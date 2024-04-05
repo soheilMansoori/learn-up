904d694ed514da7dfbc4efbf01109c4995d5f3fe
soheilMansoori <ahadmansoori369@gmail.com>
2024-04-05 14:44:48 +0330

    add sweetalert to project and create conatct form and fix it

--- src/pages/ContactUs/components/ContactForm/ContactForm.jsx
+++ src/pages/ContactUs/components/ContactForm/ContactForm.jsx
@@ -1,9 +1,19 @@
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import './ContactForm.css'
+import { useNavigate } from 'react-router-dom';
+import swal from 'sweetalert';
+import './ContactForm.css';
 
 export default function ContactForm() {
     const [isSubmitting, setSubmitting] = useState(false);
+    const navigate = useNavigate();
+    // success modal
+    const showSuccessModal = () => swal({
+        icon: 'success',
+        title: "پیام شما با موفقعیت ارسال شد",
+        buttons: "بازگشت به صفحه اصلی",
+        closeOnClickOutside: false,
+    });
 
     // form configs
     const {
@@ -43,103 +53,108 @@ export default function ContactForm() {
             .then(res => res.json())
             .then(data => {
                 setSubmitting(false);
-                reset()
+                reset() // reset form data
+                // show modal and redirect to home page
+                showSuccessModal()
+                    .then(() => navigate('/'))
             }).catch(error => console.log('error => ', error.message))
     };
 
     return (
-        <div className="col-lg-8 col-md-7">
-            <div className="prc_wrap">
-                <div className="prc_wrap_header">
-                    <h4 className="property_block_title">فرم ثبت نام</h4>
+        <>
+            <div className="col-lg-8 col-md-7">
+                <div className="prc_wrap">
+                    <div className="prc_wrap_header">
+                        <h4 className="property_block_title">فرم ثبت نام</h4>
+                    </div>
+                    <form onSubmit={handleSubmit(sendFormToServer)} className="prc_wrap-body">
+                        <div className="row">
+                            <div className="col-lg-6 col-md-12">
+                                <div className="form-group">
+                                    <label>نام کامل</label>
+                                    <input
+                                        type="text"
+                                        className="form-control simple"
+                                        name="username"
+                                        {...register(
+                                            "username",
+                                            {
+                                                required: { value: true, message: 'وارد کردن نام کاربری الزامی میباشد' },
+                                                minLength: { value: 5, message: 'نام کاربری شما حداکثر باید ۵ کلمه باشد' },
+                                                maxLength: { value: 12, message: 'نام کاربری شما باید حداقل ۱۲ کلمه باشد' }
+                                            }
+                                        )}
+                                    />
+                                    <div className="text-danger my-2">
+                                        {errors.username && errors.username.message}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-6 col-md-12">
+                                <div className="form-group">
+                                    <label>ایمیل</label>
+                                    <input
+                                        type="email"
+                                        className="form-control simple"
+                                        name="email"
+                                        {...register(
+                                            "email",
+                                            {
+                                                required: { value: true, message: 'وارد کردن ایمیل الزامی میباشد' },
+                                                pattern: { value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, message: "ایمیل وارد  شده معتبر نمیباشد" }
+                                            }
+                                        )}
+                                    />
+                                    <div className="text-danger my-2">
+                                        {errors.email && errors.email.message}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="form-group">
+                            <label>عنوان</label>
+                            <input
+                                type="text"
+                                className="form-control simple"
+                                name='title'
+                                {...register(
+                                    "title",
+                                    {
+                                        required: { value: true, message: 'وارد کردن عنوان الزامی میباشد' },
+                                        min: { value: 5, message: "عنوان باید حداقل ۵ کلمه باشد" },
+                                        max: { value: 30, message: "عنوان باید حداکثر ۳۰ کلمه باشد" },
+                                    }
+                                )}
+                            />
+                            <div className="text-danger my-2">
+                                {errors.title && errors.title.message}
+                            </div>
+                        </div>
+
+                        <div className="form-group">
+                            <label>پیام</label>
+                            <textarea
+                                className="form-control simple"
+                                name='message'
+                                {...register(
+                                    "message",
+                                    {
+                                        required: { value: true, message: 'وارد کردن متن پیام الزامی باشد' },
+                                    }
+                                )}
+                            />
+                            <div className="text-danger my-2">
+                                {errors.message && errors.message.message}
+                            </div>
+                        </div>
+
+                        <div className="form-group">
+                            <button type="submit" className="btn btn-theme" disabled={isSubmitting}>{isSubmitting ? "در حال ارسال ..." : "ارسال درخواست"}</button>
+                        </div>
+                    </form>
                 </div>
-                <form onSubmit={handleSubmit(sendFormToServer)} className="prc_wrap-body">
-                    <div className="row">
-                        <div className="col-lg-6 col-md-12">
-                            <div className="form-group">
-                                <label>نام کامل</label>
-                                <input
-                                    type="text"
-                                    className="form-control simple"
-                                    name="username"
-                                    {...register(
-                                        "username",
-                                        {
-                                            required: { value: true, message: 'وارد کردن نام کاربری الزامی میباشد' },
-                                            minLength: { value: 5, message: 'نام کاربری شما حداکثر باید ۵ کلمه باشد' },
-                                            maxLength: { value: 12, message: 'نام کاربری شما باید حداقل ۱۲ کلمه باشد' }
-                                        }
-                                    )}
-                                />
-                                <div className="text-danger my-2">
-                                    {errors.username && errors.username.message}
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 col-md-12">
-                            <div className="form-group">
-                                <label>ایمیل</label>
-                                <input
-                                    type="email"
-                                    className="form-control simple"
-                                    name="email"
-                                    {...register(
-                                        "email",
-                                        {
-                                            required: { value: true, message: 'وارد کردن ایمیل الزامی میباشد' },
-                                            pattern: { value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, message: "ایمیل وارد  شده معتبر نمیباشد" }
-                                        }
-                                    )}
-                                />
-                                <div className="text-danger my-2">
-                                    {errors.email && errors.email.message}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="form-group">
-                        <label>عنوان</label>
-                        <input
-                            type="text"
-                            className="form-control simple"
-                            name='title'
-                            {...register(
-                                "title",
-                                {
-                                    required: { value: true, message: 'وارد کردن عنوان الزامی میباشد' },
-                                    min: { value: 5, message: "عنوان باید حداقل ۵ کلمه باشد" },
-                                    max: { value: 30, message: "عنوان باید حداکثر ۳۰ کلمه باشد" },
-                                }
-                            )}
-                        />
-                        <div className="text-danger my-2">
-                            {errors.title && errors.title.message}
-                        </div>
-                    </div>
-
-                    <div className="form-group">
-                        <label>پیام</label>
-                        <textarea
-                            className="form-control simple"
-                            name='message'
-                            {...register(
-                                "message",
-                                {
-                                    required: { value: true, message: 'وارد کردن متن پیام الزامی باشد' },
-                                }
-                            )}
-                        />
-                        <div className="text-danger my-2">
-                            {errors.message && errors.message.message}
-                        </div>
-                    </div>
-
-                    <div className="form-group">
-                        <button type="submit" className="btn btn-theme" disabled={isSubmitting}>{isSubmitting ? "در حال ارسال ..." : "ارسال درخواست"}</button>
-                    </div>
-                </form>
             </div>
-        </div>
+        </>
     )
 }
