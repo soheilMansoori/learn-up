5636a956de804f9508e87059e22d3126eba49933
soheilMansoori <ahadmansoori369@gmail.com>
2024-05-14 17:55:00 +0330

    fix BookOverview component and fill datas with api datas

--- src/pages/BookDetails/components/BookOverview/BookOverview.jsx
+++ src/pages/BookDetails/components/BookOverview/BookOverview.jsx
@@ -1,6 +1,21 @@
+import { useState } from 'react';
 import './BookOverview.css';
 
-export default function BookOverview() {
+export default function BookOverview({ id, bookCode, bookName, category, description, image, isNew, isVip, price, score }) {
+    const [bookCount, setBookCount] = useState(1);
+    // handleChange
+    const handleChange = event => {
+        const inputValue = event.target.value
+        setBookCount(inputValue)
+    };
+    // add to favorite
+    const addToFavorite = () => {
+        console.log("book add to favorite", id);
+    };
+    // add to basket
+    const addBookToBasket = () => {
+        console.log("book add to basket", id);
+    };
     return (
         <section className="pr_detail">
             <div className="container">
@@ -9,43 +24,40 @@ export default function BookOverview() {
                         <div className="woocommerce single_product quick_view_wrap">
                             <div className="woo-single-images">
                                 <div className="feature-style-single">
-                                    <img src="/images/books/book-1.jpg" alt="product-book-06" width="570" />
+                                    <img src={image} width="570" height="500" />
                                 </div>
                             </div>
                             <div className="summary entry-summary">
                                 <div className="woo_inner">
-                                    <div className="shop_status sell">جدید</div>
+                                    {isNew && <div className="shop_status sell">جدید</div>}
+                                    {isVip && <div className="shop_status hot">ویژه</div>}
                                     <h2 className="woo_product_title">
-                                        <a href="#">کتاب تست شیمی دوازدهم جامع خیلی سبز</a>
+                                        <a>{bookName}</a>
                                     </h2>
                                     <ul className="woo_info">
-                                        <li><strong>دسته بندی: </strong>متوسطه دوم</li>
-                                        <li><strong>کد: </strong>#CDP0147</li>
+                                        <li><strong>دسته بندی: </strong>{category}</li>
+                                        <li><strong>کد: </strong>{bookCode}</li>
                                     </ul>
                                     <div className="woo_price_rate">
                                         <div className="woo_rating">
-                                            <i className="fas fa-star filled"></i>
-                                            <i className="fas fa-star filled"></i>
-                                            <i className="fas fa-star filled"></i>
-                                            <i className="fas fa-star filled"></i>
-                                            <i className="fas fa-star"></i>
+                                            {Array.from({ length: 5 }, (item, index) => index).map(item => <i className={`fas fa-star ${score > item && "filled"}`} key={item} />)}
                                         </div>
                                         <div className="woo_price_fix">
-                                            <h3 className="mb-0 theme-cl">149000 تومان</h3>
+                                            <h3 className="mb-0 theme-cl">{Number(price)?.toLocaleString()} تومان</h3>
                                         </div>
                                     </div>
-                                    <div className="woo_short_desc">
-                                        <p>برای بسیاری از معلمان دانشجو، چشم انداز مواجهه با اولین تجربه کلاسی خود بسیار زیاد است. این کتاب دیدگاهی واقع گرایانه از آنچه در کلاس با آن ها روبرو خواهد شد ارائه می دهد، اما همچنین مهارت هایی را که برای تبدیل شدن به معلمان حرفه ای و متفکر به آن ها نیاز دارند، ارائه می دهد.</p>
-                                    </div>
+                                    <div className="woo_short_desc" style={{ textAlign: 'justify' }}>{description}</div>
                                     <div className="quantity-button-wrapper">
                                         <label>تعداد</label>
                                         <div className="quantity">
-                                            <input type="number" className="form-control qty" min="1" value="1" title="Qty" placeholder="" inputmode="numeric" />
+                                            <input value={bookCount} onChange={handleChange} type="number" className="form-control qty" min="1" max="10" inputMode='numeric' />
                                         </div>
                                     </div>
-                                    <div className="woo_buttons">
-                                        <button type="submit" className="btn btn-theme2">افزودن به سبد</button>
-                                        <a href="#" className="btn woo_btn_light" data-toggle="tooltip" data-placement="top" title="" data-original-title="افزودن به علاقه مندی"><i className="fa fa-heart"></i></a>
+                                    <div className="row woo_buttons mr-1 ml-1">
+                                        <button onClick={addBookToBasket} type="button" className="col-12 col-lg-5 btn btn-theme2">افزودن به سبد</button>
+                                        <a onClick={addToFavorite} className="col-12 col-lg-6 btn woo_btn_light">
+                                            افزودن به علاقه مندی ها <i className="fa fa-heart mr-2" />
+                                        </a>
                                     </div>
                                 </div>
                             </div>
