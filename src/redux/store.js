516946b2be573f57a4fe1e17697d2d00f706cca7
soheilMansoori <ahadmansoori369@gmail.com>
2024-05-14 21:07:14 +0330

    add userBasket reducer in redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { userReducer } from './reducers/userReducer';
+import { userBasketReducer } from './reducers/userBasketReducer';
 
 const store = configureStore({
     devTools: true,
     reducer: {
         user: userReducer,
+        userBasket: userBasketReducer
     }
 });
 
