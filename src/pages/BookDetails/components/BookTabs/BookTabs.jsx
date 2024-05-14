01b69789df1a0e089117091f671a31d45cb87826
soheilMansoori <ahadmansoori369@gmail.com>
2024-05-14 19:50:23 +0330

    fix BookTabs component fix api

--- src/pages/BookDetails/components/BookTabs/BookTabs.jsx
+++ src/pages/BookDetails/components/BookTabs/BookTabs.jsx
@@ -1,9 +1,9 @@
 import { Tabs, Tab } from 'react-bootstrap'
-import './BookTabs.css';
 import CommentBox from '../../../../components/Comments/CommentBox/CommentBox';
 import SendComment from '../../../../components/Comments/SendComment/SendComment';
+import './BookTabs.css';
 
-export default function BookTabs() {
+export default function BookTabs({ description, specifications = [], comments = [] }) {
     return (
         <section className="bg-light">
             <div className="container">
@@ -12,48 +12,45 @@ export default function BookTabs() {
                         <div className="property_block_wrap_header">
                             <Tabs defaultActiveKey="descriptions-tab" className="nav nav-tabs customize-tab tabs_creative" id="myTab" role="tablist">
                                 <Tab title="توضیحات" eventKey="descriptions-tab" className="nav-item">
-                                    <div className='edu_wraper'>
-                                        <p>
-                                            برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد.
-                                        </p>
-                                        <p>
-                                            در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
-                                        </p>
-                                    </div>
+                                    {description ? (
+                                        <div className='edu_wraper'>{description}</div>
+                                    ) : (
+                                        <div className='alert alert-success'>توضیحاتی وجود ندارد</div>
+                                    )}
                                 </Tab>
-                                <Tab title="مشخصات" eventKey="Specifications-tab" className="nav-item">
-                                    <div className="table-responsive edu_wraper">
-                                        <table className="table">
-                                            <tbody>
-                                                <tr>
-                                                    <th scope="row">نوع درسنامه</th>
-                                                    <td>درسنامه کامل</td>
-                                                </tr>
-                                                <tr>
-                                                    <th scope="row">وزن</th>
-                                                    <td>125کیلوگرم</td>
-                                                </tr>
-                                                <tr>
-                                                    <th scope="row">نوع سوالات</th>
-                                                    <td>تستی</td>
-                                                </tr>
-                                                <tr>
-                                                    <th scope="row">نوع پاسخنامه</th>
-                                                    <td>پاسخنامه تشریحی دارد</td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
+                                <Tab title="مشخصات" eventKey="specifications-tab" className="nav-item">
+                                    {specifications.length ? (
+                                        <div className="table-responsive edu_wraper">
+                                            <table className="table">
+                                                <tbody>
+                                                    {specifications.map(item => (
+                                                        <tr key={item.id}>
+                                                            <th scope="row">{item.th}</th>
+                                                            <td>{item.td}</td>
+                                                        </tr>
+                                                    ))}
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    ) : (
+                                        <div className='alert alert-success'>اطلاعاتی برای نمایش  وجود ندارد</div>
+                                    )}
                                 </Tab>
                                 <Tab title="نظرات" eventKey="comments-tab" className="nav-item">
                                     <div className='edu_wraper'>
                                         <SendComment />
-                                        <div className="alert alert-success my-5">
-                                            هنوز نظری ثبت نشده است.
-                                        </div>
-                                        <div className='reviews-comments-wrap'>
-                                            <CommentBox />
-                                        </div>
+                                        {comments.length ? (
+                                            <div className='reviews-comments-wrap'>
+                                                {comments.map(comment => {
+                                                    <CommentBox {...comment} key={comment.id} />
+                                                })}
+                                            </div>
+
+                                        ) : (
+                                            <div className="alert alert-success my-5">
+                                                هنوز نظری ثبت نشده است.
+                                            </div>
+                                        )}
                                     </div>
                                 </Tab>
                             </Tabs>
@@ -61,6 +58,6 @@ export default function BookTabs() {
                     </div>
                 </div>
             </div>
-        </section>
+        </section >
     );
 }
